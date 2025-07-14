f68bd4f537bc7045b59ad3d3b8bd8e3306afd8f2
Vivek Singh <singhvivek357480@gmail.com>
2025-07-14 10:52:04 +0530

    added user data

---
+++ lib/utils/user_data.dart
@@ -0,0 +1,28 @@
+List<String> followingList = [
+  'Virat Kohli',
+  'Kane Williamson',
+  'Rohit Sharma',
+  'MS Dhoni',
+  'Jasprit Bumrah',
+  'Steve Smith',
+  'KL Rahul',
+  'Hardik Pandya',
+];
+
+List<String> followersList = [
+  'Virat Kohli',
+  'Kane Williamson',
+  'Rohit Sharma',
+  'MS Dhoni',
+  'Jasprit Bumrah',
+  'Steve Smith',
+  'KL Rahul',
+  'Hardik Pandya',
+];
+
+List<String> sentList = [
+  'Virat Kohli',
+  'Kane Williamson',
+  'Rohit Sharma',
+  'Steve Smith',
+];
